449f3988430794f0dff704ee24596e1c768ca737
han900204 <54014070+han900204@users.noreply.github.com>
2022-04-18 19:12:35 -0700

    updates

--- client/App.js
+++ client/App.js
@@ -3,12 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FuelEfficiency from './components/FuelEfficiency';
 import MobileNet from './components/MobileNet';
 import './style/index.scss';
+import logo from './img/logo.svg';
 
 const App = () => {
 	return (
 		<Router>
 			<div className='container'>
-				<h1>Welcome to TensorFlow</h1>
+				<img src={logo} className='logo'></img>
+				<div className='link'>
+					<a href='/fuel_efficiency'>Regression Analysis Demo</a>
+				</div>
+				<div className='link'>
+					<a href='/mobilenet'>Image Recognition Demo</a>
+				</div>
 				<Routes>
 					<Route exact path='/fuel_efficiency' element={<FuelEfficiency />} />
 					<Route exact path='/mobilenet' element={<MobileNet />} />
